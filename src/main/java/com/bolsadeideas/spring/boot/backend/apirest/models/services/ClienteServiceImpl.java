cacd58d09f771bce935aad0ede350448578d1d87
Adolfo de la Rosa Peña <adolfodelarosa2012@gmail.com>
2020-01-11 17:46:00 +0100

    34. Añadiendo las clases Repository y Service de la lógica de negocio

---
+++ src/main/java/com/bolsadeideas/spring/boot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -0,0 +1,27 @@
+package com.bolsadeideas.spring.boot.backend.apirest.models.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bolsadeideas.spring.boot.backend.apirest.models.dao.IClienteDao;
+import com.bolsadeideas.spring.boot.backend.apirest.models.entity.Cliente;
+
+//Anotar como Service para marcarla como una clase de Servicio
+@Service
+public class ClienteServiceImpl implements IClienteService{
+
+	//Inyectar (Inyección de dependdencias) el ClienteDao
+	@Autowired
+	private IClienteDao clienteDao;
+	
+	@Override
+	@Transactional(readOnly = true) //Permite manejar transacción en el método y como es un Select será sólo de lectura
+	public List<Cliente> findAll() {
+		//Como findAll() retorna un Iterable le hacemos un CAST
+		return (List<Cliente>) clienteDao.findAll();
+	}
+
+}
